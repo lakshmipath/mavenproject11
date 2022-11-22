d24459f3082f8923bc5cc4b97de55f4508f6fa4e
lakshmipath <pathi.devops75@gmail.com>
2022-11-22 11:26:39 +0530

    test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,10 @@ public class Test
 {
   public static void main(String args[])
   {
-    System.out.println("welcome");
+    System.out.println("welcome to devops");
+    System.out.println("welcome to git");
   }
+  
+
 }
+
